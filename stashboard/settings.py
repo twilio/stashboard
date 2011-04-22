1f1a29c9af03fa0eb67aa52784c2ed7dd5111194
Kyle Conroy <kyle.j.conroy@gmail.com>
2011-04-22 07:39:22 -0700

    Remove logging from settings

--- stashboard/settings.py
+++ stashboard/settings.py
@@ -1,13 +1,10 @@
 import os
-import logging
 
 DEBUG = False
 
 SITE_NAME = "Stashboard"
-SITE_URL = "http://stashbooard.appspot.com"
 SITE_AUTHOR = "Colonel Mustache"
+SITE_URL = "http://stashbooard.appspot.com"
 PUBSUBHUBBUB_URL = "http://pubsub.example.com"
 
 TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), "templates"))
-
-logging.error(TEMPLATE_DIRS)
