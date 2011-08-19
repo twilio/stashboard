3a14a03481ed7c6e9d92cd09726cb0d897d80a06
Kyle Conroy <kyle.j.conroy@gmail.com>
2011-08-19 11:07:00 -0700

    Update testing classes to be more DRY

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,58 +6,56 @@ import requests
 from google.appengine.ext import testbed
 from stashboard.main import application
 from webtest import TestApp
+import oauth2 as oauth
 
 class HTTPTest(unittest.TestCase):
     """Easily test HTTP requests against a URL"""
     base = "http://localhost"
 
-    def get(self, url, **kwargs):
+    def request(self, method, url, **kwargs):
         url = self.base + url
-        return requests.get(url, **kwargs)
+        return requests.request(method, url, **kwargs)
+
+    def get(self, url, **kwargs):
+        return self.request("GET", url, **kwargs)
 
     def post(self, url, **kwargs):
-        url = self.base + url
-        return requests.post(url, **kwargs)
+        return self.request("POST", url, **kwargs)
 
     def put(self, url, **kwargs):
-        url = self.base + url
-        return requests.put(url, **kwargs)
+        return self.request("PUT", url, **kwargs)
 
     def delete(self, url, **kwargs):
-        url = self.base + url
-        return requests.delete(url, **kwargs)
+        return self.request("DELETE", url, **kwargs)
 
     def head(self, url, **kwargs):
-        url = self.base + url
-        return requests.head(url, **kwargs)
+        return self.request("HEAD", url, **kwargs)
 
 
-class WSGITest(unittest.TestCase):
+class WSGITest(HTTPTest):
     """Easily test HTTP requests against a URL"""
 
-    def get(self, url, **kwargs):
-        return self.app.get(url, **kwargs)
-
-    def post(self, url, **kwargs):
-        return self.app.post(url, **kwargs)
-
-    def put(self, url, **kwargs):
-        return self.app.put(url, **kwargs)
-
-    def delete(self, url, **kwargs):
-        return self.app.delete(url, **kwargs)
-
-    def head(self, url, **kwargs):
-        return self.app.head(url, **kwargs)
-
+    def request(self, method, url, **kwargs):
+        return self.app.request(method, url, **kwargs)
 
 class StashboardTest(WSGITest):
 
     def setUp(self):
         self.app = TestApp(application())
         self.testbed = testbed.Testbed()
+        self.testbed.setup_env(auth_domain="testbed")
+        self.authenticate("123", "a@b.c", admin=True)
+
         self.testbed.activate()
+        self.testbed.init_memcache_stub()
+        self.testbed.init_taskqueue_stub()
         self.testbed.init_datastore_v3_stub()
+        self.testbed.init_user_stub()
+
+    def authenticate(self, name, email, admin=False):
+        admin_value = "1" if admin else "0"
+        self.testbed.setup_env(user_is_admin=admin_value, user_email=email,
+                               user_id=name, overwrite=True)
 
     def tearDown(self):
         self.testbed.deactivate()
@@ -83,6 +81,13 @@ class PublicServicesTest(StashboardTest):
 
 
 class ServicesTest(StashboardTest):
+
+    def test_create_service_name(self):
+        """Services should 400 without a name"""
+        response = self.post("/admin/api/v1/services",
+                             data={"description": "An example service API",
+                                   "name": "Some Random Name"})
+        self.assertEquals(response.status_code, 201)
 
     def test_missing_service_name(self):
         """Services should 400 without a name"""
